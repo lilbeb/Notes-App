74496a69922d7fcfef8a44f3990d63b1c2b53a98
lilbeb <orangeee04@gmail.com>
2024-03-07 12:47:18 +0300

    delete main storyboard

---
+++ Notes-App/Notes-App/Controllers/NoteViewController.swift
@@ -0,0 +1,12 @@
+//
+//  NoteViewController.swift
+//  Notes-App
+//
+//  Created by Элина Борисова on 07.03.2024.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+    
+}
---
+++ Notes-App/Notes-App/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes-App
+//
+//  Created by Элина Борисова on 07.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
